c74cdba5a044a680cd466aa5d71331b8e667d3c8
Rémi Vezy <VEZY@users.noreply.github.com>
2026-05-11 11:59:48 +0200

    Create script for making the usms.xml

---
+++ 1-code/2-create_stics_simulations.R
@@ -0,0 +1,4 @@
+# Purpose: Create the stics simulations for the different treatments defined in the design of experiment
+
+# Read the design of experiment defined in 1-design_of_experiment.R
+df_doe <- read.csv("2-outputs/doe.csv")
