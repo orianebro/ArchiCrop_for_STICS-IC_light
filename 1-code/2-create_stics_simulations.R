c7a92e20280035bec73f9b9cc0a1044c9d86920b
Rémi Vezy <VEZY@users.noreply.github.com>
2026-05-11 12:04:17 +0200

    Update 2-create_stics_simulations.R

--- 1-code/2-create_stics_simulations.R
+++ 1-code/2-create_stics_simulations.R
@@ -2,3 +2,9 @@
 
 # Read the design of experiment defined in 1-design_of_experiment.R
 df_doe <- read.csv("2-outputs/doe.csv")
+
+# Make a file that links the species and design to true values (e.g. maize middel interrow = XXm)
+
+# Create the tec files
+
+# Create the usms in the usms.xml file, each named after the doe row, and linking to the tec file needed.
